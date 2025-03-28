27e33dc666caa29821d70342a04fefc582e8512f
cfrotaru <c.f.rotaru@gmail.com>
2025-03-28 22:53:13 +0200

    - Implemented #CC4

--- 11-Arrays-Bankist/starter/CodingChallanges.js
+++ 11-Arrays-Bankist/starter/CodingChallanges.js
@@ -26,4 +26,65 @@ export default class CodingChallange {
     console.log(`Human Ages: ${humanAges}`);
     console.log(`Average human ages: ${average}`);
   }
+
+  checkDogsEatingPortions(dogs) {
+    //1 //2
+    dogs.forEach(dog => {
+      dog.recommendedFood = dog.weight ** 0.75 * 28;
+      if (dog.owners.some(owner => owner === 'Sarah')) {
+        if (dog.curFood < dog.recommendedFood * 0.9) {
+          console.log(`Sarah's dog eats too little`);
+        } else if (dog.curFood > dog.recommendedFood * 1.1) {
+          console.log(`Sarah's dog eats too much`);
+        } else console.log(`Sarah's dog eats a normal portion of food`);
+      }
+    });
+
+    //3
+    const ownersEatTooMuch = dogs
+      .filter(dog => dog.curFood > dog.recommendedFood * 1.1)
+      .reduce((owners, dog) => owners.concat(dog.owners), []);
+    const ownersEatTooLittle = dogs
+      .filter(dog => dog.curFood < dog.recommendedFood * 0.9)
+      .reduce((owners, dog) => owners.concat(dog.owners), []);
+
+    console.log(`ownersEatTooMuch: `, ownersEatTooMuch);
+    console.log(`ownersEatTooLittle: `, ownersEatTooLittle);
+
+    //4
+    console.log(`${ownersEatTooLittle.join(' and ')}' dogs eat too little!`);
+    console.log(`${ownersEatTooMuch.join(' and ')}' dogs eat too much!`);
+
+    //5
+    console.log(
+      dogs.some(dog => dog.recommendedFood === dog.curFood)
+        ? `#5 Yes, there is a dog eating the exact recommended amount of food`
+        : `#5 No, there is no dog eating the exact recommended amount of food`
+    );
+
+    //6
+    console.log(
+      dogs.some(
+        dog =>
+          dog.curFood <= dog.recommendedFood * 1.1 &&
+          dog.curFood >= dog.recommendedFood * 0.9
+      )
+        ? `#6 Yes, there is a dog eating an ok amount of food`
+        : `#6 No, there is no dog eating an ok amount of food`
+    );
+
+    //7
+    const dogsEatingOk = dogs.filter(
+      dog =>
+        dog.curFood <= dog.recommendedFood * 1.1 &&
+        dog.curFood >= dog.recommendedFood * 0.9
+    );
+    console.log(`#7: `, dogsEatingOk);
+
+    //8
+    const dogsArr = dogs.slice();
+    console.log(`dogsArr: `, dogsArr, `dogs: `, dogs);
+    dogsArr.sort((a, b) => a.recommendedFood - b.recommendedFood);
+    console.log(`#8: `, dogsArr);
+  }
 }
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -30,3 +30,14 @@ const cc3testData2 = [16, 6, 10, 5, 6, 1, 4];
 
 cc.calcAverageHumanAge(cc3testData1);
 cc.calcAverageHumanAge(cc3testData2);
+
+//cc#4
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+cc.checkDogsEatingPortions(dogs);
