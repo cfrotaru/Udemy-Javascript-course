747838f0cf7bae09f3270b29f6e05747b6809626
cfrotaru <c.f.rotaru@gmail.com>
2025-04-25 16:05:56 +0300

    - Add and display OSM using leaflet

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,25 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+    var map = L.map('map').setView(coords, 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+
+    console.log(`https://www.google.com/maps/@${latitude},${longitude},20z`);
+  },
+  function () {
+    alert(`Could not get your position!`);
+  }
+);
