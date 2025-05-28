3649012df93cfd22f31ad59dbcc6165d397bf76a
cfrotaru <c.f.rotaru@gmail.com>
2025-05-28 18:47:20 +0300

    - Add form to search for a specific country

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -2,11 +2,21 @@
 import { Countries } from './countries.js';
 
 const btn = document.querySelector('.btn-country');
+const inputCountry = document.querySelector('.input-country');
+const btnRender = document.querySelector('.btn-render-countries');
+const searchForm = document.querySelector('.country-search');
 const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 const countries = new Countries();
 const countryName = 'argentina';
 
-btn.addEventListener('click', function () {
-  countries.getCountryDataByNameFromApi(countryName);
+// btnRender.addEventListener('click', function () {
+//   countriesContainer.innerHTML = '';
+//   countries.getCountryDataByNameFromApi(inputCountry.value);
+// });
+
+searchForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  countriesContainer.innerHTML = '';
+  countries.getCountryDataByNameFromApi(inputCountry.value);
 });
