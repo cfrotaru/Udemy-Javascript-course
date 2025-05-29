92c2e4d62ecf363ef86e6f4c4f72483b5b2f9054
cfrotaru <c.f.rotaru@gmail.com>
2025-05-30 01:32:37 +0300

    - promisifying exercise

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -12,7 +12,8 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 const countries = new Countries();
 const countryName = 'argentina';
-
+const images = document.querySelector('.images');
+let image;
 const clearAndRenderCurrentCountry = function () {
   countriesContainer.innerHTML = '';
   countries.renderCurrentCountryAndNeighbours.call(countries);
@@ -30,3 +31,64 @@ countryCoordsForm.addEventListener('submit', function (e) {
   countriesContainer.innerHTML = '';
   countries.getCountryAndNeighboursByLatLng(inputLat.value, inputLng.value);
 });
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening');
+  setTimeout(function () {
+    if (Math.random() > 0.5) {
+      resolve('You win');
+    } else {
+      reject(new Error('You lost'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+//Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('I waited for 1 second'));
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!'))
+  .then(x => console.error(x))
+  .catch(err => console.log(err));
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+    img.addEventListener('load', function () {
+      image = img;
+      images.append(image);
+      resolve(image);
+    });
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+createImage(`img/img-1.jpg`)
+  .then(() => {
+    return wait(2);
+  })
+  .then(() => {
+    image.style.display = 'none';
+    return createImage(`img/img-2.jpg`);
+  })
+  .then(() => wait(2))
+  .then(() => {
+    image.style.display = 'none';
+  })
+  .catch(err => console.log(err));
