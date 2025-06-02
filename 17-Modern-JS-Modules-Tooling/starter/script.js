425a2ad3b11bc3723a1e3461c1a19d73ce667e6c
cfrotaru <c.f.rotaru@gmail.com>
2025-06-03 02:06:24 +0300

    - Practice named/default imports/exports

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,18 @@
+//Importing module
+
+//import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+
+console.log('Importing module');
+
+//addToCart('bread', 5);
+//console.log(price, tq);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread, 5');
+// console.log(ShoppingCart.totalPrice);
+
+//import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+//console.log(price);
+
+import add from './shoppingCart.js';
+add('pizza', 2);
