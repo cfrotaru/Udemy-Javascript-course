8c535535ab496e03307a60df83cf61a94b6c97b0
cfrotaru <c.f.rotaru@gmail.com>
2025-08-13 22:49:17 +0300

    - display user generated icon on recipes created by current user

--- 18-forkify/starter/src/js/view/previewView.js
+++ 18-forkify/starter/src/js/view/previewView.js
@@ -1,4 +1,6 @@
 import View from './view';
+import icons from 'url:../../img/icons.svg';
+
 export default class PreviewView extends View {
   _data;
   _container;
@@ -23,6 +25,11 @@ export default class PreviewView extends View {
               <div class="preview__data">
                 <h4 class="preview__title">${recipe.title}</h4>
                 <p class="preview__publisher">${recipe.publisher}</p>
+                <div class="preview__user-generated ${
+                  recipe.key ? '' : 'hidden'
+                }">
+                  <svg><use href="${icons}#icon-user"></use></svg>
+                </div>
               </div>
             </a>
           </li>
