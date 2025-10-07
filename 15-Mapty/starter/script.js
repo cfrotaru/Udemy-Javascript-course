0f6d4b1541af4d61d41108806e2329707ed0643d
cfrotaru <c.f.rotaru@gmail.com>
2025-10-07 19:57:50 +0300

    - Add "Go to my location" button on map

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -41,6 +41,17 @@ class App {
   _loadMap() {
     let coords = [0, 0];
     this.#map = L.map('map').setView(coords, 5);
+    L.control
+      .locate({
+        position: 'topleft',
+        setView: 'once',
+        keepCurrentZoomLevel: false,
+        flyTo: true,
+        initialZoomLevel: 14,
+        strings: { title: 'Go to my location' },
+      })
+      .addTo(this.#map);
+
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
